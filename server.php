faf136ce71906eb94a1c3d0cb89104efc93948ba
Taras <taras.kostiuk.eleks@gmail.com>
2018-12-14 13:03:56 +0200

    Fix.

--- server.php
+++ server.php
@@ -17,13 +17,14 @@ $gpio = new Gpio([PIN_1, PIN_2]);
 $gpio->setup(PIN_1, GpioInterface::DIRECTION_OUT)
     ->setup(PIN_2, GpioInterface::DIRECTION_OUT);
 
-$loop->addPeriodicTimer(5, function () use ($gpio, &$pin2ToggleState){
-    $gpio->write(PIN_OUT, $pin2ToggleState);
-    ($pin2ToggleState) ? $pin2ToggleState = 0 : $pin2ToggleState = 1;
+$loop->addPeriodicTimer(1, function () use ($gpio, &$pin1ToggleState) {
+    $gpio->write(PIN_1, $pin1ToggleState);
+    ($pin1ToggleState) ? $pin1ToggleState = 0 : $pin1ToggleState = 1;
 });
 
-$loop->addPeriodicTimer(1, function () use ($gpio, &$pin1ToggleState) {
-    ($pin1ToggleState) ? $pin1ToggleState = 0 : $pin1ToggleState = 1;
+$loop->addPeriodicTimer(5, function () use ($gpio, &$pin2ToggleState){
+    $gpio->write(PIN_1, $pin2ToggleState);
+    ($pin2ToggleState) ? $pin2ToggleState = 0 : $pin2ToggleState = 1;
 });
 
 $loop->run();
